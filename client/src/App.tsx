3bf82c143ced959c6f9e842c3c4b31e9c62b78b2
pkowshik2020 <pkowshik@usc.edu>
2023-04-12 19:26:34 -0700

    website colors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,40 @@
-import Grid from "./Grid";
-import { PaintContextProvider } from "./paintContext";
-import Pallette from "./Pallette";
-import axios from "axios";
+import Grid from "./Grid"
+import { PaintContextProvider } from "./paintContext"
+import Pallette from "./Pallette"
+import axios from "axios"
 
 const onSubmit = async () => {
-  axios.get("http://localhost:8080/getAll/").then((response) => {
-      console.log(response.data);
+  axios
+    .get("http://localhost:8080/getAll/")
+    .then((response) => {
+      console.log(response.data)
     })
     .catch(function (error) {
-      console.log(error);
-    });
+      console.log(error)
+    })
 }
 
 function App() {
   return (
     <div>
-     <PaintContextProvider>
+      <div class="header">
+        <h1>Waterbender Drawing App</h1>
+      </div>
+      <PaintContextProvider>
         <div className="outer">
           <div className="inner">
             <Grid />
           </div>
-          <Pallette />
-          <button onClick={onSubmit}>
-            Submit Drawing
-          </button>
+          <div className="side-panel">
+            <Pallette />
+            <button onClick={onSubmit}>
+              Submit Drawing
+            </button>
+          </div>
         </div>
       </PaintContextProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
