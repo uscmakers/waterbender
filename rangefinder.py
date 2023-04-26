8530db596b130584d1c00a76b07f93ed8fe27c05
Pooja Kowshik <pkowshik@usc.edu>
2023-04-25 21:13:49 -0700

    Update rangefinder.py

--- rangefinder.py
+++ rangefinder.py
@@ -6,30 +6,60 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 #set GPIO Pins
-GPIO_TRIGGER = 2
-GPIO_ECHO = 3
+GPIO1_TRIGGER = 2
+GPIO1_ECHO = 3
+GPIO2_TRIGGER = 4
+GPIO2_ECHO = 17
 
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO1_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO1_ECHO, GPIO.IN)
+GPIO.setup(GPIO2_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO2_ECHO, GPIO.IN)
 
-def distance():
+def distance1():
     # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    GPIO.output(GPIO1_TRIGGER, True)
 
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(GPIO1_TRIGGER, False)
 
     StartTime = time.time()
     StopTime = time.time()
 
     # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
+    while GPIO.input(GPIO1_ECHO) == 0:
         StartTime = time.time()
 
     # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
+    while GPIO.input(GPIO1_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+    return distance
+
+def distance2():
+    # set Trigger to HIGH
+    GPIO.output(GPIO2_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO2_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO2_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO2_ECHO) == 1:
         StopTime = time.time()
 
     # time difference between start and arrival
@@ -42,8 +72,11 @@ def distance():
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
+            dist1 = distance1()
+            print ("Measured Distance 1 = %.1f cm" % dist1)
+
+            dist2 = distance2()
+            print ("Measured Distance 2 = %.1f cm" % dist2)
             time.sleep(1)
 
         # Reset by pressing CTRL + C
